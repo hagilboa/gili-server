ad1a2f4eb01fda003c3f76acb2cb0728ad61ef3c
hagilboa <doverhagilboa@gmail.com>
2025-09-25 09:39:24 +0300

    Delete SRC directory

--- SRC/validators.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const mobileSchema = z.string().regex(/^(?:\\+972|0)5\\d{8}$/, "מספר נייד לא תקין");
-
-export function validateMobile(m: string) {
-  return mobileSchema.safeParse(m);
-}
